9fd1ed1d19edb7db9310c35635078e2d9a38b30e
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-11-15 14:54:47 -0300

    Time: 122 ms, Memory: 43.8 MB - LeetHub

--- remove-element/remove-element.ts
+++ remove-element/remove-element.ts
@@ -1,25 +1,16 @@
 function removeElement(nums: number[], val: number): number {
     const length = nums.length - 1
+    let start = 0
     let end = length
-    let answer = 0
     
-    for (let i = 0; i < length; i++) {
-        if (nums[i] === val) {
-            [nums[end], nums[i]] = [nums[i], nums[end]]
+    while (end >= start) {
+        if (nums[start] === val) {
+            [nums[end], nums[start]] = [nums[start], nums[end]]
             end--
-            i--
-        }
-        if (end <= i) {
-            break
-        }
-    }
-    
-    for (let i = 0; i <= length; i++) {
-        if (nums[i] !== val) {
-            answer++
         } else {
-            break
+            start++
         }
     }
-    return answer
+
+    return start
 };
