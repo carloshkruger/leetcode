6d0d727e94134e0f36579f832b17d44797225098
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-11-15 14:49:57 -0300

    Time: 128 ms, Memory: 46 MB - LeetHub

---
+++ remove-element/remove-element.ts
@@ -0,0 +1,26 @@
+function removeElement(nums: number[], val: number): number {
+    const length = nums.length - 1
+    let end = length
+    let answer = 0
+    
+    for (let i = 0; i < length; i++) {
+        if (nums[i] === val) {
+            [nums[end], nums[i]] = [nums[i], nums[end]]
+            end--
+            i--
+        }
+        if (end <= i) {
+            break
+        }
+    }
+    console.log(nums)
+    
+    for (let i = 0; i <= length; i++) {
+        if (nums[i] !== val) {
+            answer++
+        } else {
+            break
+        }
+    }
+    return answer
+};
