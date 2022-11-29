4b05ae21cb1f239eb19ec082d430e33d4ab24b08
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-11-28 22:44:15 -0300

    Time: 148 ms (6.52%), Space: 45.5 MB (43.48%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.ts
+++ 0867-transpose-matrix/0867-transpose-matrix.ts
@@ -1,13 +1,13 @@
 function transpose(matrix: number[][]): number[][] {
     const answer = []
     
-    for (let i = 0; i < matrix[0].length; i++) {
-        answer.push([matrix[0][i]])
-    }
-    
-    for (let i = 1; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            answer[j].push(matrix[i][j])
+            if (answer[j]) {
+                answer[j].push(matrix[i][j])
+            } else {
+                answer[j] = [matrix[i][j]]
+            }
         }
     }
     
