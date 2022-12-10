98ea4684792a72e0f825fc0ec5de235ef4806a5c
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-12-09 21:33:45 -0300

    Time: 138 ms (82.72%), Space: 46.1 MB (62.96%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.ts
+++ 0037-sudoku-solver/0037-sudoku-solver.ts
@@ -1,6 +1,4 @@
 const BOARD_LENGTH = 9;
-const MIN_VALID_NUMBER = 1;
-const MAX_VALID_NUMBER = 9;
 const EMPTY_CELL_VALUE = '.';
 const VALID_VALUES = ['1','2','3','4','5','6','7','8','9']
 
@@ -32,15 +30,6 @@ function solve(board: string[][]): boolean {
     return true
 }
 
-// function getPossibleValues(board: string[][], row: number, col: number): number[] {
-//     let possibleValues = new Set(VALID_VALUES);
-    
-//     for (let rowIndex = 0; rowIndex < BOARD_LENGTH; rowIndex++) {
-        
-//     }
-    
-// }
-
 function isValidChoice(board: string[][], row: number, col: number, value: string): boolean {
     for (let i = 0; i < BOARD_LENGTH; i++) {
         if (board[i][col] !== EMPTY_CELL_VALUE && board[i][col] === value) {
@@ -65,69 +54,5 @@ function isValidChoice(board: string[][], row: number, col: number, value: strin
         }
     }
     
-    return true
-    
-//     for (let i = 0; i < BOARD_LENGTH; i++) {
-//         if (!isValidRow(board, i)) {
-//             return false
-//         }
-//     }
-    
-//     for (let i = 0; i < BOARD_LENGTH; i++) {
-//         if (!isValidCol(board, i)) {
-//             return false
-//         }
-//     }
-    
-//     const START_SUB_BOXES_INDEX = [[0,0],[0,3],[0,6],[3,0],[3,3],[3,6],[6,0],[6,3],[6,6]]
-    
-//     for (let i = 0; i < START_SUB_BOXES_INDEX.length; i++) {
-//         if (!isValidSubBox(board, START_SUB_BOXES_INDEX[i][0], START_SUB_BOXES_INDEX[i][1])) {
-//             return false
-//         }
-//     }
-        
-//     return true
-}
-
-function isValidRow(board: string[][], row: number): boolean {
-    const map = {}
-    for (let i = 0; i < BOARD_LENGTH; i++) {
-        if (map[board[row][i]]) {
-            return false
-        }
-        map[board[row][i]] = true
-    }
-    return true
-}
-
-function isValidCol(board: string[][], col: number): boolean {
-    const map = {}
-    for (let i = 0; i < BOARD_LENGTH; i++) {
-        if (map[board[i][col]]) {
-            return false
-        }
-        map[board[i][col]] = true
-    }
-    return true
-}
-
-function isValidSubBox(board: string[][], startSubBoxRow: number, startSubBoxCol: number): boolean {
-    const SUB_BOX_LENGTH = 3
-
-    const endSubBoxRow = startSubBoxRow + SUB_BOX_LENGTH
-    const endSubBoxCol = startSubBoxCol + SUB_BOX_LENGTH
-    
-    const map = {}
-
-    for (let rowIndex = startSubBoxRow; rowIndex < endSubBoxRow; rowIndex++) {
-        for (let colIndex = startSubBoxCol; colIndex < endSubBoxCol; colIndex++) {
-            if (map[board[rowIndex][colIndex]]) {
-                return false
-            }
-            map[board[rowIndex][colIndex]] = true   
-        }
-    }
-
     return true
 }
