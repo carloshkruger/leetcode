568c0ec6c6db0f987a2826c7b13fc76f0d958128
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-12-13 22:01:58 -0300

    Time: 115 ms (83.71%), Space: 56.2 MB (84.27%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,16 +1,13 @@
 function findMaxAverage(nums: number[], k: number): number {
     let maxAverage = Number.NEGATIVE_INFINITY
-    let subarray = []
     let startIndex = 0
     let sum = 0
     
     for (let i = 0; i < nums.length; i++) {
-        subarray.push(nums[i])
         sum += nums[i]
         
-        if (subarray.length === k) {
-            maxAverage = Math.max(maxAverage, sum / subarray.length)
-            subarray.shift()
+        if (i + 1 >= k) {
+            maxAverage = Math.max(maxAverage, sum / k)
             sum -= nums[startIndex]
             startIndex++
         }
