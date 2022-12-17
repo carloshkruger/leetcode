021fc8fb1f58914c4d264d37db33dad44cdca765
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-12-17 20:59:52 -0300

    Time: 324 ms, Memory: 47.3 MB - LeetHub

--- 4sum-ii/4sum-ii.ts
+++ 4sum-ii/4sum-ii.ts
@@ -10,8 +10,9 @@ function fourSumCount(nums1: number[], nums2: number[], nums3: number[], nums4: 
         
     for (const num3 of nums3) {
         for (const num4 of nums4) {
-            if (map.has(-num3-num4)) {
-                answer += map.get(-num3-num4)
+            const key = (num3 + num4) * -1
+            if (map.has(key)) {
+                answer += map.get(key)
             }
         }
     }
