4566cdff9a8ccc5bdf931c5e8ab9fb78614a64f9
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-12-26 11:05:35 -0300

    Time: 376 ms (73.29%), Space: 67.1 MB (40.37%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -1,25 +1,14 @@
 function dailyTemperatures(temperatures: number[]): number[] {
     const stack = []
-    const answer = new Array(temperatures.length)
-    let answerIndex = temperatures.length - 1
+    const answer = new Array(temperatures.length).fill(0)
 
-    for (let i = temperatures.length - 1; i >= 0; i--) {
+    for (let i = 0; i < temperatures.length; i++) {
         let currentTemperature = temperatures[i]
-        let poppedElementCount = 0
-        let stackSize = stack.length
-        while (stack.length && currentTemperature >= stack.at(-1).value) {
-            stack.pop()
-            poppedElementCount++
-        }
-        if (stackSize === poppedElementCount) {
-            answer[answerIndex] = 0
-        } else if (stackSize === stack.length) {
-            answer[answerIndex] = 1
-        } else {
-            answer[answerIndex] = stack.at(-1).index - i
+        while (stack.length && currentTemperature > stack.at(-1).value) {
+            const { index } = stack.pop()
+            answer[index] = i - index
         }
         stack.push({value: currentTemperature, index: i})
-        answerIndex--
     }
 
     return answer
