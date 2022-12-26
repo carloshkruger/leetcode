50cdf575288a763258832b2a00caca1708428566
Carlos Henrique Kruger <carloshkruger@gmail.com>
2022-12-25 22:40:46 -0300

    Time: 3462 ms (29.61%), Space: 68.7 MB (33.95%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -0,0 +1,25 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    const stack = []
+    const answer = []
+
+    for (let i = temperatures.length - 1; i >= 0; i--) {
+        let currentTemperature = temperatures[i]
+        let elementsPopped = []
+        let poppedElementCount = 0
+        let stackSize = stack.length
+        while (stack.length && currentTemperature >= stack.at(-1).value) {
+            stack.pop()
+            poppedElementCount++
+        }
+        if (stackSize === poppedElementCount) {
+            answer.unshift(0)
+        } else if (stackSize === stack.length) {
+            answer.unshift(1)
+        } else {
+            answer.unshift(stack.at(-1).index - i)
+        }
+        stack.push({value: currentTemperature, index: i})
+    }
+
+    return answer
+};
