1ca3ec9470b81209b04b1cfc98e5dac1f83b89b8
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-01-17 21:50:54 -0300

    Time: 103 ms (59.83%), Space: 46.1 MB (5.34%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -0,0 +1,27 @@
+function mySqrt(x: number): number {
+    if (x === 0) {
+        return 0
+    }
+    
+    let start = 0
+    let end = x
+    
+    while (start < end) {
+        const middle = (start + end) / 2
+        const value = middle * middle
+        
+        if (value === x) {
+            return middle
+        }
+        if (value > x) {
+            end = middle
+        } else {
+            start = middle
+        }
+        if (parseInt(start+'') === parseInt(end+'')) {
+            return Math.floor(start)
+        }
+    }
+
+    return -1
+};
