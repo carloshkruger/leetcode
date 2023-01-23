94c61f3baec590f6ebe7959f7bf4641b42d6d7e7
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-01-23 15:44:17 -0300

    Time: 182 ms (15.18%), Space: 50.5 MB (5.18%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,7 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false
+    }
+    
+    return s.split('').sort().join('') === t.split('').sort().join('')
+};
