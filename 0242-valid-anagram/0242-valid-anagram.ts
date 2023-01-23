985c7a4c29a0baff7d44aac5c1b8ed58d3dd6445
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-01-23 15:48:06 -0300

    Time: 114 ms (50.04%), Space: 45 MB (72.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -3,5 +3,22 @@ function isAnagram(s: string, t: string): boolean {
         return false
     }
     
-    return s.split('').sort().join('') === t.split('').sort().join('')
+    const sMap = new Map()
+    for (const letter of s) {
+        sMap.set(letter, (sMap.get(letter) ?? 0) + 1)
+    }
+    for (const letter of t) {
+        if (!sMap.has(letter) || sMap.get(letter) === 0) {
+            return false
+        }
+        sMap.set(letter, sMap.get(letter) - 1)
+    }
+    
+    for (const count of sMap.values()) {
+        if (count !== 0) {
+            return false
+        }
+    }
+        
+    return true
 };
