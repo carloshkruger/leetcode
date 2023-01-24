44c4996505840768307fb703ea9eff92fd95853e
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-01-24 11:32:22 -0300

    Time: 104 ms (93.09%), Space: 57.2 MB (15.70%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,21 +1,16 @@
 function productExceptSelf(nums: number[]): number[] {
-    const map1 = new Map()
-    const map2 = new Map()
-    
-    map1.set(0, nums[0])
-    map2.set(nums.length-1, nums.at(-1))
-    
-    let end = nums.length - 2
-    for (let start = 1; start < nums.length; start++) {
-        map1.set(start, map1.get(start-1) * nums[start])
-        map2.set(end, map2.get(end+1) * nums[end])
-        end--
-    }
-    
     const answer = []
+    let prefix = 1
+    let postfix = 1
     
     for (let i = 0; i < nums.length; i++) {
-        answer.push((map1.get(i-1) ?? 1) * (map2.get(i+1) ?? 1))
+        answer[i] = prefix
+        prefix *= nums[i]
+    }
+        
+    for (let i = nums.length - 2; i >= 0; i--) {
+        postfix *= nums[i+1]
+        answer[i] *= postfix
     }
     
     return answer
