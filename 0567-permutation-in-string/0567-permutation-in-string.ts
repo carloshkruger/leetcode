d309a596161be77c505454ff6e520d8fbc4c4599
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-01-27 14:36:29 -0300

    Time: 105 ms (74.48%), Space: 49.2 MB (47.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.ts
+++ 0567-permutation-in-string/0567-permutation-in-string.ts
@@ -1,16 +1,43 @@
 function checkInclusion(s1: string, s2: string): boolean {
-    const s1Ordered = s1.split('').sort().join('')
-    let currentString = ''
+    const s1HashMap = new Map()
+    for (const letter of s1) {
+        s1HashMap.set(letter, (s1HashMap.get(letter) ?? 0) + 1)
+    }
+
+    const currentHashMap = new Map()
+
+    let slowIndex = 0
+    let currentLength = 0
     for (let i = 0; i < s2.length; i++) {
-        currentString += s2[i]
+        currentHashMap.set(s2[i], (currentHashMap.get(s2[i]) ?? 0) + 1)
+        currentLength++
         
-        if (currentString.length === s1Ordered.length) {
-            let currentStringOrdered = currentString.split('').sort().join('')
-            if (currentStringOrdered === s1Ordered) {
+        if (currentLength === s1.length) {
+            let ok = true
+
+            for (const [key, count] of currentHashMap.entries()) {
+                if (!s1HashMap.has(key) || count > s1HashMap.get(key)) {
+                    ok = false
+                    break
+                }
+            }
+
+            if (ok) {
                 return true
             }
-            currentString = currentString.slice(1)
+
+            const currentCount = currentHashMap.get(s2[slowIndex])
+
+            if (currentCount === 1) {
+                currentHashMap.delete(s2[slowIndex])
+            } else {
+                currentHashMap.set(s2[slowIndex], currentCount - 1)
+            }
+
+            slowIndex++
+            currentLength--
         }
     }
+
     return false
 };
