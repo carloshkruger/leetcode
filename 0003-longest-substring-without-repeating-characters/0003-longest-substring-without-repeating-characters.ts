a3c96fa020593899fc379e66fca989c36fb7ef5f
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-01-28 18:01:46 -0300

    Time: 95 ms (80.91%), Space: 46.9 MB (82.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -1,16 +1,20 @@
 function lengthOfLongestSubstring(s: string): number {
-    let longest = 0
-    let startSubStringIndex = 0
-    let lastIndex = s.length - 1
-    const map = new Map()
+    const hashMap = new Map()
+    let queue = []
+    let longestSubstringLength = 0
     
-    for (let i = 0; i <= lastIndex; i++) {
-        if (map.has(s[i])) {
-            longest = Math.max(longest, i - startSubStringIndex)
-            startSubStringIndex = Math.max(startSubStringIndex, map.get(s[i]) + 1)
+    for (let i = 0; i < s.length; i++) {
+        if (hashMap.has(s[i])) {
+            longestSubstringLength = Math.max(longestSubstringLength, hashMap.size)
+            
+            while (queue.length && queue[0] !== s[i]) {
+                hashMap.delete(queue.shift())
+            }
+            queue.shift()
         }
-        map.set(s[i], i)
+        hashMap.set(s[i], i)
+        queue.push(s[i])
     }
     
-    return Math.max(longest, lastIndex - startSubStringIndex + 1)
-};
+    return Math.max(longestSubstringLength, hashMap.size)
+}
