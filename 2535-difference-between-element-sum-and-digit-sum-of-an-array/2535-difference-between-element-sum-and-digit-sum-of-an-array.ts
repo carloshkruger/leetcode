92a6433bc4fa1fc7aebe660bd3459257ef424654
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-02-16 21:26:45 -0300

    Time: 85 ms (47.57%), Space: 47.2 MB (47.09%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.ts
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.ts
@@ -4,7 +4,7 @@ function differenceOfSum(nums: number[]): number {
     
     for (const num of nums) {
         elementSum += num
-        digitSum += num.toString().split('').map(Number).reduce((a,b) => a+b, 0)
+        digitSum += num.toString().split('').reduce((a,b) => a + Number(b), 0)
     }
     
     return elementSum - digitSum
