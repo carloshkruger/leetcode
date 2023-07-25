53481875a3d510c8490ddada51781a945bfe6230
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-07-24 21:20:39 -0300

    Time: 50 ms, Memory: 42.3 MB - LeetHub

--- plus-one/plus-one.ts
+++ plus-one/plus-one.ts
@@ -1,3 +1,22 @@
 function plusOne(digits: number[]): number[] {
-    return (BigInt(digits.join('')) + 1n).toString().split('').map(Number)
+    if (digits.at(-1) < 9) {
+        digits[digits.length - 1] = digits[digits.length - 1] + 1
+        return digits
+    }
+    
+    let rest = 0
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0
+            rest = 1
+        } else if (rest > 0) {
+            digits[i] = digits[i] + rest
+            rest = 0
+            break
+        }
+    }
+    if (rest > 0) {
+        digits.unshift(1)
+    }
+    return digits
 };
