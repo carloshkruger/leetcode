ce9683f5c30cf3bb0f3c0ed06c059b577aa926a9
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-07-27 20:40:50 -0300

    Time: 91 ms, Memory: 50 MB - LeetHub

--- reverse-string/reverse-string.ts
+++ reverse-string/reverse-string.ts
@@ -2,15 +2,14 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    let begin = 0;
-    let end = s.length - 1
+    let leftIndex = 0
+    let rightIndex = s.length - 1
     
-    while (begin < end) {
-        const temp = s[end]
-        s[end] = s[begin]
-        s[begin] = temp
-        
-        begin++
-        end--
+    while (leftIndex < rightIndex) {
+        const temp = s[rightIndex]
+        s[rightIndex] = s[leftIndex]
+        s[leftIndex] = temp
+        leftIndex++
+        rightIndex--
     }
 };
