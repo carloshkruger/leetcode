4c6551e3badcf9ea97658c0b291c5ba69810f430
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-01 21:25:46 -0300

    Time: 2840 ms (79.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+select salesperson.name
+  from salesperson
+ where salesperson.sales_id not in (
+    select sales_id
+      from orders
+      join company
+        on company.com_id = orders.com_id
+     where company.name = "RED"
+ )
