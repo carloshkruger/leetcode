3ccd3a222445e7aa8149292aa36e4b6f387ae760
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-11 15:51:11 -0300

    Time: 57 ms (87.89%) | Memory: 45 MB (54.70%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null
+    let currentNode = head
+
+    while (currentNode) {
+        const temp = currentNode.next
+        currentNode.next = prev
+        prev = currentNode
+        currentNode = temp
+    }
+
+    return prev
+};
