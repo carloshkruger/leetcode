8c3bad3ce699a41fcc237aa492be74233e8abbe9
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-23 14:18:50 -0300

    Time: 86 ms (41.13%) | Memory: 52.4 MB (42.75%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.ts
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -3,12 +3,7 @@ function maxSubArray(nums: number[]): number {
     let currentSum = 0
 
     for (let i = 0; i < nums.length; i++) {
-        if (currentSum < 0 && nums[i] > currentSum) {
-            currentSum = nums[i]
-        } else {
-            currentSum += nums[i]
-        }
-
+        currentSum = Math.max(currentSum, 0) + nums[i]
         largestSum = Math.max(largestSum, currentSum)
     }
 
