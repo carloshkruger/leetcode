033150fe9c1ba48afbc1889748d67cdcb4b4756a
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-28 17:02:46 -0300

    Time: 56 ms (73.52%) | Memory: 44.4 MB (34.70%) - LeetSync

--- 62-unique-paths/unique-paths.ts
+++ 62-unique-paths/unique-paths.ts
@@ -6,13 +6,8 @@ function uniquePaths(m: number, n: number, cache = {}): number {
   if (m === 1 && n === 1) {
     return 1
   }
-  if (m === 1) {
-    cache[cacheKey] = uniquePaths(m, n - 1, cache)
-    return cache[cacheKey]
-  }
-  if (n === 1) {
-    cache[cacheKey] = uniquePaths(m - 1, n, cache)
-    return cache[cacheKey]
+  if (m < 1 || n < 1) {
+    return 0
   }
   cache[cacheKey] = uniquePaths(m, n - 1, cache) + uniquePaths(m - 1, n, cache)
   return cache[cacheKey]
