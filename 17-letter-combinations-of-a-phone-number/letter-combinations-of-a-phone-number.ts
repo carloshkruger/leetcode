26ae6150696c9c840ca6a2faacbda038fa4237d5
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-29 11:54:55 -0300

    Time: 56 ms (59.01%) | Memory: 42.8 MB (76.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
@@ -1,32 +1,34 @@
 function letterCombinations(digits: string): string[] {
-    if (!digits.length) {
-        return []
+  if (!digits.length) {
+    return []
+  }
+
+  const answer = []
+  const map = {
+    2: 'abc',
+    3: 'def', 
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
+  }
+
+  function helper(digitIndex: number, current: string): void {
+    if (current.length === digits.length) {
+      answer.push(current)
+      return
     }
 
-    const map = {
-        '2': 'abc',
-        '3': 'def',
-        '4': 'ghi',
-        '5': 'jkl',
-        '6': 'mno',
-        '7': 'pqrs',
-        '8': 'tuv',
-        '9': 'wxyz' 
+    const letters = map[digits[digitIndex]]
+
+    for (let i = 0; i < letters.length; i++) {
+      helper(digitIndex + 1, current + letters[i])
     }
-    const answer = []
+  }
 
-    function helper(currentCombination: string, digitIndex: number) {
-        if (digitIndex === digits.length) {
-            answer.push(currentCombination)
-            return
-        }
+  helper(0, "")
 
-        for (let i = 0; i < map[digits[digitIndex]].length; i++) {
-            helper(`${currentCombination}${map[digits[digitIndex]][i]}`, digitIndex + 1)
-        }
-    }
-
-    helper('', 0)
-
-    return answer
+  return answer
 };
