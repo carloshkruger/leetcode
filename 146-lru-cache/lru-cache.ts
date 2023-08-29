f3d3cdb4c913aa1e1be0c323650788b2afa30240
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-29 17:35:15 -0300

    Time: 501 ms (93.23%) | Memory: 124.8 MB (69.38%) - LeetSync

--- 146-lru-cache/lru-cache.ts
+++ 146-lru-cache/lru-cache.ts
@@ -1,6 +1,8 @@
 class LRUCache {
   private capacity: number
-  private data = new Map<number, number>()
+  private data = new Map<number, LinkedListNode>()
+  private head: LinkedListNode | null = null
+  private tail: LinkedListNode | null = null
 
   constructor(capacity: number) {
     this.capacity = capacity
@@ -8,21 +10,89 @@ class LRUCache {
 
   get(key: number): number {
     if (this.data.has(key)) {
-      const value = this.data.get(key)
-      this.data.delete(key)
-      this.data.set(key, value)
-      return value
+      const node = this.data.get(key)
+      this.updateNode(node)
+      return node.data
     }
     return -1
   }
 
   put(key: number, value: number): void {
-    if (this.data.size === this.capacity && !this.data.has(key)) {
-      const firstItemKey = this.data.keys().next().value
-      this.data.delete(firstItemKey)
+    if (this.data.has(key)) {
+      const node = this.data.get(key)
+      this.updateNode(node)
+      node.data = value
+      this.data.set(key, node)
+    } else {
+      const newNode = new LinkedListNode(value, key)
+
+      if (this.isFull()) {
+        this.data.delete(this.head.key)
+        this.deleteFirstNode()
+      }
+
+      this.updateNode(newNode)
+      this.data.set(key, newNode)
     }
-    this.data.delete(key)
-    this.data.set(key, value)
+  }
+
+  private isFull(): boolean {
+    return this.data.size === this.capacity
+  }
+
+  private deleteFirstNode(): void {
+    if (this.head === this.tail) {
+      this.head = null
+      this.tail = null
+      return
+    }
+    this.head = this.head.next
+    this.head.prev = null
+  }
+
+  private updateNode(node: LinkedListNode): void {
+    if (!this.head) {
+      this.head = node
+      this.tail = node
+      return
+    }
+    if (this.tail === node) {
+      return
+    }
+    if (this.head === node) {
+      this.deleteFirstNode()
+      this.append(node)
+      return
+    }
+
+    if (node.next) {
+      node.next.prev = node.prev
+    }
+    if (node.prev) {
+      node.prev.next = node.next
+    }
+
+    this.append(node)
+  }
+
+  private append(node: LinkedListNode): void {
+    node.prev = this.tail
+    this.tail.next = node
+    this.tail = node    
+  }
+}
+
+class LinkedListNode {
+  public data: number
+  public key: number
+  public prev: LinkedListNode | null
+  public next: LinkedListNode | null
+
+  constructor(data: number, key: number, prev?: LinkedListNode, next?: LinkedListNode) {
+    this.data = data
+    this.key = key
+    this.prev = prev ?? null
+    this.next = next ?? null
   }
 }
 
