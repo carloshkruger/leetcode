be095d81f91f3abce3a740e98fd59e5c668dd38e
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-08-30 16:58:06 -0300

    Time: 58 ms (53.42%) | Memory: 42.2 MB (91.93%) - LeetSync

--- 198-house-robber/house-robber.ts
+++ 198-house-robber/house-robber.ts
@@ -8,12 +8,9 @@ function rob(nums: number[]): number {
     if (index >= nums.length) {
       return 0
     }
-    cache[index] = Math.max(
-      nums[index] + Math.max(helper(index + 2), helper(index + 3)),
-      helper(index + 1)
-    )
+    cache[index] = nums[index] + Math.max(helper(index + 2), helper(index + 3))
     return cache[index]
   }
 
-  return helper(0)
+  return Math.max(helper(0), helper(1))
 };
