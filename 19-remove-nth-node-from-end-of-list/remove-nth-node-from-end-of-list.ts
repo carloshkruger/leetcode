d53075ebf4be16c2f30e20177bad9b55fe3065fb
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-01 15:25:12 -0300

    Time: 64 ms (44.42%) | Memory: 44.7 MB (36.40%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  if (!head) {
+    return null
+  }
+
+  let current = head
+  let nodeToRemove = head
+  let previousNodeToRemove = head
+  let count = 0
+
+  while (current) {
+    count++
+    current = current.next
+    if (count >= n && current) {
+      previousNodeToRemove = nodeToRemove
+      nodeToRemove = nodeToRemove.next
+    }
+  }
+
+  if (count === 1) {
+    return null
+  }
+  if (previousNodeToRemove === nodeToRemove) {
+    return nodeToRemove.next
+  }
+
+  previousNodeToRemove.next = nodeToRemove.next
+
+  return head
+};
