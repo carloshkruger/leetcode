39ed0015752962d0d08582eb4d34d2a102977852
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-01 17:05:24 -0300

    Time: 130 ms (8.26%) | Memory: 53.5 MB (5.22%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.ts
@@ -0,0 +1,46 @@
+function findOrder(numCourses: number, prerequisites: number[][]): number[] {
+  const inDegree = new Map()
+  const graph = new Map()
+
+  for (let i = 0; i < numCourses; i++) {
+    inDegree.set(i, 0)
+  }
+
+  for (const [destiny, source] of prerequisites) {
+    inDegree.set(destiny, inDegree.get(destiny) + 1)
+    const list = graph.get(source) ?? []
+    list.push(destiny)
+    graph.set(source, list)
+  }
+  
+
+  const queue = []
+  for (const [key, value] of inDegree.entries()) {
+    if (value === 0) {
+      queue.push(key)
+    }
+  }
+  console.log({inDegree, queue, graph})
+  
+  const answer = []
+  while (queue.length) {
+    const current = queue.pop()
+    answer.push(current)
+    if (graph.has(current)) {
+      for (const item of graph.get(current)) {
+        const itemDegree = inDegree.get(item)
+        inDegree.set(item, itemDegree - 1)
+        if (itemDegree === 1) {
+          queue.push(item)
+        } 
+      }
+    }
+  }
+  for (const value of inDegree.values()) {
+    if (value > 0) {
+      return []
+    }
+  }
+
+  return answer
+};
