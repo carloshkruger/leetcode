805eda661cb58eb9d66b67f4dad710e077f93c11
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-04 13:31:20 -0300

    Time: 80 ms (91.50%) | Memory: 46.1 MB (87.58%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.ts
@@ -0,0 +1,52 @@
+function minWindow(s: string, t: string): string {
+  if (t.length > s.length) {
+    return ''
+  }
+
+  const tHashCount = new Map()
+  for (const letter of t) {
+    tHashCount.set(letter, (tHashCount.get(letter) ?? 0) + 1)
+  }
+
+  let smalestLength = Infinity
+  let smalestString = ''
+  let windowStart = 0
+  let matches = 0
+
+  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+    const letter = s[windowEnd]
+    if (tHashCount.has(letter)) {
+      const count = tHashCount.get(letter)
+      if (count === 1) {
+        matches++
+      }
+      tHashCount.set(letter, count - 1)
+    }
+  
+    while (matches === tHashCount.size) {
+      if (windowEnd - windowStart + 1 < smalestLength) {
+        smalestLength = windowEnd - windowStart + 1
+        smalestString = s.substring(windowStart, windowEnd+1) 
+      }
+      if (tHashCount.has(s[windowStart])) {
+        const count = tHashCount.get(s[windowStart])
+        tHashCount.set(s[windowStart], count + 1)
+        if (count >= 0) {
+          matches--
+        }
+      }
+      windowStart++
+    }
+  }
+
+  return smalestString
+};
+
+function test(tHashCount, currentHash): boolean {
+    for (const [key, value] of tHashCount) {
+      if (!currentHash.has(key) || currentHash.get(key) !== value) {
+        return false
+      }
+    }
+    return true
+}
