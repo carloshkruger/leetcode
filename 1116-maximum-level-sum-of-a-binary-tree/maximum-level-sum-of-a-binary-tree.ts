048dec330d3aa35449b6b2d0fd170d891826eecf
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-06 11:41:36 -0300

    Time: 133 ms (76.12%) | Memory: 75.8 MB (30.60%) - LeetSync

---
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxLevelSum(root: TreeNode | null): number {
+  if (!root) {
+    return 0
+  }
+  const queue = [root]
+  let maximumSum = -Infinity
+  let smallestLevel = 0
+  let currentLevel = 0
+  while (queue.length) {
+    currentLevel++
+    const levelSize = queue.length
+    let levelSum = 0
+    for (let i = 0; i < levelSize; i++) {
+      const currentNode = queue.shift()
+      levelSum += currentNode.val
+      if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right)
+      }
+    }
+    if (levelSum > maximumSum) {
+      maximumSum = levelSum
+      smallestLevel = currentLevel
+    }
+  }
+  return smallestLevel
+};
