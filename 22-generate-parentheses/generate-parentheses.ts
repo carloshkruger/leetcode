b6b596e5c41af2058ede3d1bce8082470fdafb62
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-06 17:44:12 -0300

    Time: 37 ms (99.88%) | Memory: 44.7 MB (61.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.ts
+++ 22-generate-parentheses/generate-parentheses.ts
@@ -1,32 +1,20 @@
 function generateParenthesis(n: number): string[] {
   const answer = []
-  const queue = [{
-    str: '',
-    openCount: 0,
-    closeCount: 0
-  }]
 
-  while (queue.length) {
-    const current = queue.shift()
-    if (current.openCount === n && current.closeCount === n) {
-      answer.push(current.str)
-    } else {
-      if (current.openCount < n) {
-        queue.push({
-          str: `${current.str}(`,
-          openCount: current.openCount + 1,
-          closeCount: current.closeCount
-        })
-      }
-      if (current.openCount > current.closeCount) {
-        queue.push({
-          str: `${current.str})`,
-          openCount: current.openCount,
-          closeCount: current.closeCount + 1
-        })
-      }
+  function helper(current: string, openCount: number, closeCount: number) {
+    if (openCount === 0 && closeCount === 0) {
+      answer.push(current)
+      return
+    }
+    if (openCount > 0) {
+      helper(`${current}(`, openCount - 1, closeCount)
+    }
+    if (openCount < closeCount) {
+      helper(`${current})`, openCount, closeCount - 1)
     }
   }
+
+  helper('', n, n)
 
   return answer
 };
