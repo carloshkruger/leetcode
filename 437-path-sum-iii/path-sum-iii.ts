8d9cf305fcb2446eb41175a4d950f4b4ab0c570c
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-06 14:41:28 -0300

    Time: 88 ms (60.63%) | Memory: 81.4 MB (14.17%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathSum(root: TreeNode | null, targetSum: number): number {
+  if (!root) {
+    return 0
+  }
+
+  function dfs(node: TreeNode | null, currentPath: number[]): number {
+    if (!node) {
+      return 0 
+    }
+
+    currentPath.push(node.val)
+    let pathSum = 0
+    let pathCount = 0
+    for (let i = currentPath.length-1; i >= 0; i--) {
+      pathSum += currentPath[i]
+      if (pathSum === targetSum) {
+        pathCount++
+      }
+    }
+
+    pathCount += dfs(node.left, [...currentPath])
+    pathCount += dfs(node.right, [...currentPath])
+
+    currentPath.pop()
+    return pathCount
+  }
+
+  return dfs(root, [])
+};
