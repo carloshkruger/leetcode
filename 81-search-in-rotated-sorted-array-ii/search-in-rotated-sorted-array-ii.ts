809cf671a093bf461541e47385f8eb6a7e9aa71c
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-07 13:00:47 -0300

    Time: 59 ms (54.22%) | Memory: 44.7 MB (26.71%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.ts
@@ -0,0 +1,31 @@
+function search(nums: number[], target: number): boolean {
+  let start = 0
+  let end = nums.length - 1
+
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2)
+
+    if (nums[mid] === target) {
+      return true
+    }
+
+    if (nums[start] === nums[mid] && nums[end] === nums[mid]) {
+      start++
+      end--
+    } else if (nums[start] <= nums[mid]) {
+      if (target >= nums[start] && target < nums[mid]) {
+        end = mid - 1
+      } else {
+        start = mid + 1
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[end]) {
+        start = mid + 1
+      } else {
+        end = mid - 1
+      }
+    }
+  }
+
+  return false
+};
