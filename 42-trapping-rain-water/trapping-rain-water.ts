1aabecdd212e703d6e390cd8ada2423848e9c404
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-11 11:13:42 -0300

    Time: 66 ms (62.36%) | Memory: 45.4 MB (57.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.ts
+++ 42-trapping-rain-water/trapping-rain-water.ts
@@ -1,20 +1,19 @@
 function trap(height: number[]): number {
   let trappedWater = 0
-  let leftIndex = 0
-  let rightIndex = height.length - 1
-  let maxLeftHeight = height[leftIndex]
-  let maxRightHeight = height[rightIndex]
+  const stack = []
 
-  while (leftIndex <= rightIndex) {
-    if (maxLeftHeight <= maxRightHeight) {
-      maxLeftHeight = Math.max(maxLeftHeight, height[leftIndex])
-      trappedWater += maxLeftHeight - height[leftIndex]
-      leftIndex++
-    } else {
-      maxRightHeight = Math.max(maxRightHeight, height[rightIndex])
-      trappedWater += maxRightHeight - height[rightIndex]
-      rightIndex--
+  for (let i = 0; i < height.length; i++) {
+    while (stack.length && height[stack[stack.length-1]] < height[i]) {
+      const middleIndex = stack.pop()
+      if (!stack.length) {
+        break
+      }
+      const leftHeight = height[stack[stack.length-1]]
+      const minHeight = Math.min(leftHeight, height[i]) - height[middleIndex]
+      const distance = i - stack[stack.length-1] - 1
+      trappedWater += distance * minHeight
     }
+    stack.push(i)
   }
 
   return trappedWater
