0d9c6ecff085bbcfc863f4c65d06672f4c130dbc
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-11 11:33:29 -0300

    Time: 57 ms (71.29%) | Memory: 44.3 MB (91.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.ts
+++ 22-generate-parentheses/generate-parentheses.ts
@@ -1,20 +1,32 @@
 function generateParenthesis(n: number): string[] {
   const answer = []
+  const stack = [{
+    str: '',
+    leftCount: 0,
+    rightCount: 0 
+  }]
 
-  function helper(current: string, leftCount: number, rightCount: number): void {
-    if (leftCount === n && rightCount === n) {
-      answer.push(current)
-      return
-    }
-    if (leftCount < n) {
-      helper(`${current}(`, leftCount+1, rightCount)
-    }
-    if (rightCount < leftCount) {
-      helper(`${current})`, leftCount, rightCount+1)
+  while (stack.length) {
+    const current = stack.pop()
+    if (current.leftCount === n && current.rightCount === n) {
+      answer.push(current.str)
+    } else {
+      if (current.leftCount < n) {
+        stack.push({
+          str: `${current.str}(`,
+          leftCount: current.leftCount + 1,
+          rightCount: current.rightCount
+        })
+      }
+      if (current.leftCount > current.rightCount) {
+        stack.push({
+          str: `${current.str})`,
+          leftCount: current.leftCount,
+          rightCount: current.rightCount + 1
+        })
+      }
     }
   }
-
-  helper('', 0, 0)
 
   return answer
 };
