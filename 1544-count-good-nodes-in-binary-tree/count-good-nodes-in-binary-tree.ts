a78e49b02fc7206dd66786efdd926f11909d8060
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-12 09:26:23 -0300

    Time: 113 ms (73.15%) | Memory: 79.2 MB (33.46%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function goodNodes(root: TreeNode | null): number {
+  if (!root) {
+    return 0
+  }
+
+  let goodNodesCount = 0
+
+  function helper(root: TreeNode | null, maxValue: number) {
+    if (!root) {
+      return
+    }
+    if (root.val >= maxValue) {
+      goodNodesCount++
+    }
+
+    const newMaxValue = Math.max(maxValue, root.val)
+
+    helper(root.left, newMaxValue)
+    helper(root.right, newMaxValue)
+  }
+
+  helper(root, root.val)
+
+  return goodNodesCount
+};
