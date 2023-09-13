48c83f9d9fe746b22d6a2b368db3aeaf42611151
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-13 17:21:37 -0300

    Time: 149 ms (11.89%) | Memory: 59.8 MB (19.77%) - LeetSync

--- 200-number-of-islands/number-of-islands.ts
+++ 200-number-of-islands/number-of-islands.ts
@@ -1,42 +1,34 @@
 function numIslands(grid: string[][]): number {
-    const visited = new Set<string>()
-    let count = 0
+  const visited = new Set<string>()
+  let islandCount = 0
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (isIsland(grid, i, j, visited)) {
-                count++
-            }
-        }
+  function dfs(row: number, col: number): boolean {
+    const value = grid[row]?.[col]
+    const cacheKey = `${row},${col}`
+    const isOutOfBounds = value === undefined
+    const isWater = value === '0'
+
+    if (isOutOfBounds || visited.has(cacheKey) || isWater) {
+      return false
     }
 
-    return count
-};
+    visited.add(cacheKey)
 
-function isIsland(grid: string[][], row: number, col: number, visited: Set<string>): boolean {
-    const isRowOutOfBounds = row < 0 || row >= grid.length
-    if (isRowOutOfBounds) {
-        return false
-    }
-
-    const isColOutOfBounds = col < 0 || col >= grid[row].length
-    if (isColOutOfBounds) {
-        return false
-    }
-
-    if (grid[row][col] === '0') {
-        return false
-    }
-    const key = `${row},${col}`
-    if (visited.has(key)) {
-        return false
-    }
-    visited.add(key)
-
-    isIsland(grid, row + 1, col, visited)
-    isIsland(grid, row - 1, col, visited)
-    isIsland(grid, row, col + 1, visited)
-    isIsland(grid, row, col - 1, visited)
+    dfs(row+1, col)
+    dfs(row-1, col)
+    dfs(row, col+1)
+    dfs(row, col-1)
 
     return true
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (dfs(i, j)) {
+        islandCount++
+      }
+    }
+  }
+
+  return islandCount
 }
