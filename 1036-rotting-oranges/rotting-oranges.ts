a6c5e0d279b4f662a91d804d4430d8928eb86e1b
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-14 10:28:13 -0300

    Time: 71 ms (65.77%) | Memory: 46.5 MB (55.03%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.ts
+++ 1036-rotting-oranges/rotting-oranges.ts
@@ -1,46 +1,66 @@
 function orangesRotting(grid: number[][]): number {
-    const directions = [[-1,0], [1,0], [0,-1], [0,1]]
-    const visited = new Set()
+  const directions = [[-1,0], [1,0], [0,-1], [0,1]]
+  const visited = new Set()
 
-    let { rotten: queue, freshCount } = findAllRottenOrangesAndFreshCount(grid)
-    let minutes = 0
+  let queue = findAllRottenOranges(grid)
+  let freshCount = countFreshOranges(grid)
+  let minutes = -1
 
-    while (queue.length) {
-        const [row, col, distance] = queue.shift()
+  while (queue.length) {
+    minutes++
 
-        for (const [i, j] of directions) {
-            const key = `${row+i},${col+j}`
-            const neighborCell = grid[row+i]?.[col+j]
+    const queueSize = queue.length
+    for (let i = 0; i < queueSize; i++) {
+      const [currentRow, currentCol] = queue.shift()
 
-            if (neighborCell === 1 && !visited.has(key)) {
-                queue.push([row+i, col+j, distance+1])
-                visited.add(key)
-                minutes = Math.max(minutes, distance+1)
-                freshCount--
-            }
+      for (const direction of directions) {
+        const newRow = currentRow + direction[0]
+        const newCol = currentCol + direction[1]
+
+        if (grid[newRow]?.[newCol] === 1 && !visited.has(`${newRow},${newCol}`)) {
+          freshCount--
+          queue.push([newRow, newCol])
+          visited.add(`${newRow},${newCol}`)
         }
-    }
+      }
 
-    if (freshCount > 0) {
-        return -1
+      visited.add(`${currentRow},${currentCol}`)
     }
+  }
 
-    return minutes
+  if (freshCount > 0) {
+    return -1
+  }
+  if (minutes === -1) {
+    return 0
+  }
+  return minutes
 };
 
-function findAllRottenOrangesAndFreshCount(grid: number[][]): { rotten: number[][], freshCount: number} {
-    const rotten = []
-    let freshCount = 0
+function findAllRottenOranges(grid: number[][]): number[][] {
+  const rotten = []
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === 2) {
-                rotten.push([i, j, 0])
-            } else if (grid[i][j] === 1) {
-                freshCount++
-            }
-        }
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 2) {
+        rotten.push([i, j])
+      }
     }
+  }
 
-    return { rotten, freshCount }
+  return rotten
+}
+
+function countFreshOranges(grid: number[][]): number {
+  let freshCount = 0
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        freshCount++
+      }
+    }
+  }
+
+  return freshCount
 }
