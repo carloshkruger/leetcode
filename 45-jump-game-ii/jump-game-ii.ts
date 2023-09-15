7ffe4d7fdd20d33440692e466e11f847538c8967
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-15 15:36:02 -0300

    Time: 196 ms (22.90%) | Memory: 45.3 MB (30.00%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.ts
+++ 45-jump-game-ii/jump-game-ii.ts
@@ -4,7 +4,7 @@ function jump(nums: number[]): number {
 
   for (let i = list.length-2; i >= 0; i--) {
     const currentValue = Math.min(nums[i], list.length-1-i)
-    for (let j = currentValue; j >= 1; j--) {
+    for (let j = 0; j <= currentValue; j++) {
       if (list[j+i] !== Infinity) {
         list[i] = Math.min(list[i], 1+list[j+i])
         if (list[i] === 1) {
