45e75891954110dad1db063b70436af18cf5d6bb
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-15 17:11:09 -0300

    Time: 54 ms (70.40%) | Memory: 44.6 MB (38.31%) - LeetSync

--- 100-same-tree/same-tree.ts
+++ 100-same-tree/same-tree.ts
@@ -13,11 +13,13 @@
  */
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    if (!p && !q) {
-        return true
-    }
-    if (!p || !q || p.val !== q.val) {
-        return false
-    }
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
-};
+  if (!p && !q) {
+    return true
+  }
+  if (!p || !q || p.val !== q.val) {
+    return false
+  }
+  return p.val === q.val
+    && isSameTree(p.left, q.left)
+    && isSameTree(p.right, q.right)
+}
