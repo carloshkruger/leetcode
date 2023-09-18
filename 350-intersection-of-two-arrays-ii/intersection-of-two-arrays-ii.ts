9aeee2c69a03c256b26714e5e5e37365916ebdf2
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-18 17:16:41 -0300

    Time: 58 ms (72.32%) | Memory: 44.6 MB (82.49%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
@@ -1,26 +1,21 @@
 function intersect(nums1: number[], nums2: number[]): number[] {
   const mapNums1 = new Map()
-  const mapNums2 = new Map()
   const answer = []
   
   for (const num of nums1) {
     mapNums1.set(num, (mapNums1.get(num) ?? 0) + 1)
   }
   for (const num of nums2) {
-    mapNums2.set(num, (mapNums2.get(num) ?? 0) + 1)
-  }
-                    
-  let smallest = mapNums1.size < mapNums2.size ? mapNums1 : mapNums2
-  let biggest = mapNums1.size < mapNums2.size ? mapNums2 : mapNums1
-
-  for (const [num, count] of smallest.entries()) {
-    if (biggest.has(num)) {
-      const quantity = Math.min(count, biggest.get(num))
-      for (let i = 0; i < quantity; i++) {
-        answer.push(num)
+    if (mapNums1.has(num)) {
+      answer.push(num)
+      const count = mapNums1.get(num)
+      if (count === 1) {
+        mapNums1.delete(num)
+      } else {
+        mapNums1.set(num, count - 1)
       }
     }
   }
-          
+
   return answer
 };
