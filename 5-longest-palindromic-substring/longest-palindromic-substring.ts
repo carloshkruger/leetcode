c158df24d3f287d0b4152f31579429db17648ac5
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-20 15:58:36 -0300

    Time: 74 ms (89.63%) | Memory: 45.4 MB (82.06%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.ts
+++ 5-longest-palindromic-substring/longest-palindromic-substring.ts
@@ -1,33 +1,29 @@
 function longestPalindrome(s: string): string {
-  if (s.length < 2) {
-    return s
-  }
-
   let longest = ''
 
-  for (let i = 0; i <= s.length - 1; i++) {
-    let leftIndex = i - 1
-    let rightIndex = i + 1
+  for (let i = 0; i < s.length; i++) {
+    let leftIndex = i-1
+    let rightIndex = i+1
 
     while (leftIndex >= 0 && rightIndex < s.length && s[leftIndex] === s[rightIndex]) {
       leftIndex--
       rightIndex++
     }
-    let maybeLongest = s.substring(leftIndex+1, rightIndex)
-    if (maybeLongest.length > longest.length) {
-      longest = maybeLongest
+
+    if (rightIndex - leftIndex - 1 > longest.length) {
+      longest = s.substring(leftIndex+1, rightIndex)
     }
 
     leftIndex = i
-    rightIndex = i + 1
+    rightIndex = i+1
 
     while (leftIndex >= 0 && rightIndex < s.length && s[leftIndex] === s[rightIndex]) {
       leftIndex--
       rightIndex++
     }
-    maybeLongest = s.substring(leftIndex+1, rightIndex)
-    if (maybeLongest.length > longest.length) {
-      longest = maybeLongest
+
+    if (rightIndex - leftIndex - 1 > longest.length) {
+      longest = s.substring(leftIndex+1, rightIndex)
     }
   }
 
