134d38dbcf5e6cc8ed76030e7258dea0bb987783
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-09-19 22:04:28 -0300

    Time: 45 ms (97.50%) | Memory: 44.4 MB (29.31%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  if (!root) {
+    return []
+  }
+
+  const answer = []
+  const stack = []
+  let current = root
+
+  while (current || stack.length) {
+    while (current) {
+      stack.push(current)
+      current = current.left
+    }
+    const node = stack.pop()
+    answer.push(node.val)
+    current = node.right
+  }
+
+  return answer
+};
