9fa1e855da86b8c400ab8c6ae3c28c3796f9dab3
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-19 17:42:19 -0300

    Time: 59 ms (59.71%) | Memory: 44.5 MB (52.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.ts
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -1,19 +1,22 @@
 function isValid(s: string): boolean {
     const stack = []
-    for (let char of s) {
+
+    for (const char of s) {
         if (['(', '{', '['].includes(char)) {
             stack.push(char)
-        } else if (stack.length === 0) {
-            return false
         } else {
             const top = stack.pop()
-            if (top === '(' && char !== ')'
-                || top === '{' && char !== '}'
-                || top === '[' && char !== ']'
-            ) {
+            if (char === '}' && top !== '{') {
+                return false
+            }
+            if (char === ')' && top !== '(') {
+                return false
+            }
+            if (char === ']' && top !== '[') {
                 return false
             }
         }
     }
+
     return stack.length === 0
 };
