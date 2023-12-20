419e991692a0ef78faa0c4d1fb16a7170f77d8ca
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-20 14:54:05 -0300

    Time: 192 ms (23.57%) | Memory: 51.6 MB (24.78%) - LeetSync

--- 322-coin-change/coin-change.ts
+++ 322-coin-change/coin-change.ts
@@ -1,29 +1,27 @@
 function coinChange(coins: number[], amount: number): number {
-  const cache = {}
+    const cache = {}
 
-  function helper (amount: number): number {
-    if (amount in cache) {
-      return cache[amount]
-    }
-    if (amount < 0) {
-      return -1
-    }
-    if (amount === 0) {
-      return 0
+    function helper(amount: number): number {
+        if (amount in cache) {
+            return cache[amount]
+        }
+        if (amount < 0) {
+            return -1
+        }
+        if (amount === 0) {
+            return 0
+        }
+
+        let min = Infinity
+        for (const coin of coins) {
+            const result = helper(amount - coin)
+            if (result !== -1) {
+                min = Math.min(min, result + 1)
+            }
+        }
+        cache[amount] = min === Infinity ? -1 : min
+        return cache[amount]
     }
 
-    let min = Infinity
-  
-    for (const coin of coins) {
-      const result = helper(amount - coin)
-      if (result !== -1) {
-        min = Math.min(min, result+1)
-      }
-    }
-
-    cache[amount] = min === Infinity ? -1 : min 
-    return cache[amount]
-  }
-
-  return helper(amount)
+    return helper(amount)
 };
