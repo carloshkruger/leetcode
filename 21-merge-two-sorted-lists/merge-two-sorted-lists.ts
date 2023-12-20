f81a773306019e9eb6a99443015428521d83952b
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-20 10:04:42 -0300

    Time: 74 ms (11.83%) | Memory: 45.3 MB (24.43%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -11,25 +11,25 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    let finalList = new ListNode()
-    let finalListTail = finalList
+  const newList = new ListNode()
+  let tail = newList
 
-    while (list1 && list2) {
-        if (list1.val <= list2.val) {
-            finalListTail.next = list1
-            list1 = list1.next
-        } else {
-            finalListTail.next = list2
-            list2 = list2.next
-        }
-        finalListTail = finalListTail.next
-    }
-
-    if (list1) {
-        finalListTail.next = list1
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      tail.next = list1
+      list1 = list1.next
     } else {
-        finalListTail.next = list2
+      tail.next = list2
+      list2 = list2.next
     }
+    tail = tail.next
+  }
 
-    return finalList.next
+  if (list1) {
+    tail.next = list1
+  } else {
+    tail.next = list2
+  }
+
+  return newList.next
 };
