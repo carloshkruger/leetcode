28a3f463f5671d7f3f98bbbf45ee9daf155ca8a0
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-20 10:25:21 -0300

    Time: 66 ms (83.60%) | Memory: 44.9 MB (70.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,3 +1,24 @@
 function isAnagram(s: string, t: string): boolean {
-    return s.split('').sort().join('') === t.split('').sort().join('')
+  const hashMap = new Map()
+  for (const char of s) {
+    hashMap.set(char, (hashMap.get(char) ?? 0) + 1)
+  }
+
+  for (const char of t) {
+    if (!hashMap.has(char)) {
+      return false
+    }
+    const value = hashMap.get(char)
+    if (value === 1) {
+      hashMap.delete(char)
+    } else {
+      hashMap.set(char, value - 1)
+    }
+  }
+
+  if (hashMap.size > 0) {
+    return false
+  }
+
+  return true
 };
