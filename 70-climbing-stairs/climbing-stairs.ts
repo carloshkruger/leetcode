d4af4abc2c181fc4b046a9dbe72689389ae10fb3
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-20 10:57:16 -0300

    Time: 54 ms (58.96%) | Memory: 43.2 MB (21.35%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,19 +1,20 @@
 function climbStairs(n: number): number {
-  const cache = {}
+    let cache = {}
 
-  function helper(n: number): number {
-    if (n in cache) {
-      return cache[n]
+    function dp(n: number): number {
+        if (n < 0) {
+            return 0
+        }
+        if (n === 0) {
+            return 1
+        }
+        if (cache[n]) {
+            return cache[n]
+        }
+        
+        cache[n] = dp(n - 1) + dp(n - 2)
+        return cache[n]
     }
-    if (n < 0) {
-      return 0
-    }
-    if (n === 0) {
-      return 1
-    }
-    cache[n] = helper(n-1) + helper(n-2)
-    return cache[n]
-  }
 
-  return helper(n)
+    return dp(n)
 };
