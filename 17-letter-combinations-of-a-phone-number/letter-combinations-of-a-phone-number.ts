7665755bea5157fd674706f1a337a3427520672f
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-27 13:37:48 -0300

    Time: 55 ms (57.61%) | Memory: 42.9 MB (76.58%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
@@ -1,34 +1,34 @@
 function letterCombinations(digits: string): string[] {
-  if (!digits.length) {
-    return []
-  }
-
-  const answer = []
-  const map = {
-    2: 'abc',
-    3: 'def', 
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz'
-  }
-
-  function backtrack(current: string, index: number) {
-    if (current.length === digits.length) {
-      answer.push(current)
-      return
+    if (!digits.length) {
+        return []
     }
 
-    const letters = map[digits[index]]
-
-    for (let i = 0; i < letters.length; i++) {
-      backtrack(current + letters[i], index + 1)
+    const map = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
     }
-  }
 
-  backtrack('', 0)
+    const combinations = []
 
-  return answer
+    const generateCombinations = (index: number, current: string) => {
+        if (current.length === digits.length) {
+            combinations.push(current)
+            return
+        }
+
+        const letters = map[digits[index]]
+        for (const char of letters) {
+            generateCombinations(index + 1, current + char)
+        }
+    }
+
+    generateCombinations(0, '')
+
+    return combinations
 };
