d416cc7dad7b209249f8006399e02bbcc326b39c
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-28 17:54:33 -0300

    Time: 71 ms (88.39%) | Memory: 45.7 MB (72.97%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.ts
+++ 5-longest-palindromic-substring/longest-palindromic-substring.ts
@@ -1,30 +1,20 @@
 function longestPalindrome(s: string): string {
   let longest = ''
 
+  function checkLongest(leftIndex: number, rightIndex: number): void {
+    while (leftIndex >= 0 && rightIndex < s.length && s[leftIndex] === s[rightIndex]) {
+      leftIndex--
+      rightIndex++
+    }
+
+    if (rightIndex - leftIndex - 1 > longest.length) {
+      longest = s.substring(leftIndex + 1, rightIndex)
+    }
+  }
+
   for (let i = 0; i < s.length; i++) {
-    let leftIndex = i-1
-    let rightIndex = i+1
-
-    while (leftIndex >= 0 && rightIndex < s.length && s[leftIndex] === s[rightIndex]) {
-      leftIndex--
-      rightIndex++
-    }
-
-    if (rightIndex - leftIndex - 1 > longest.length) {
-      longest = s.substring(leftIndex+1, rightIndex)
-    }
-
-    leftIndex = i
-    rightIndex = i+1
-
-    while (leftIndex >= 0 && rightIndex < s.length && s[leftIndex] === s[rightIndex]) {
-      leftIndex--
-      rightIndex++
-    }
-
-    if (rightIndex - leftIndex - 1 > longest.length) {
-      longest = s.substring(leftIndex+1, rightIndex)
-    }
+    checkLongest(i - 1, i + 1)
+    checkLongest(i - 1, i)
   }
 
   return longest
