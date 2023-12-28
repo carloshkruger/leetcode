0f9e63432f78ce70988cd68be6801db0a5cf6bce
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-28 16:40:04 -0300

    Time: 201 ms (28.75%) | Memory: 51.2 MB (83.75%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.ts
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.ts
@@ -1,46 +1,54 @@
 function pacificAtlantic(heights: number[][]): number[][] {
-  if (!heights.length) {
-    return []
-  }
+    const visitedPacific = new Set<string>()
+    const visitedAtlantic = new Set<string>()
+    const m = heights.length
+    const n = heights[0].length 
 
-  const pacific = new Map<string, number[]>()
-  const atlantic = new Map<string, number[]>()
+    function dfs(row: number, col: number, visited: Set<string>, lastValue: number): void {
+        const isOutOfBounds = row < 0
+            || col < 0
+            || row >= m
+            || col >= n
+        if (isOutOfBounds) {
+            return
+        }
 
-  function dfs(row: number, col: number, visited: Map<string, number[]>, prevValue: number) {
-    const currentValue = heights[row]?.[col]
-    const isOutOfBounds = currentValue === undefined
-    const key = `${row},${col}`
-  
-    if (isOutOfBounds || currentValue < prevValue || visited.has(key)) {
-      return
+        const value = heights[row][col]
+
+        if (value < lastValue) {
+            return
+        }
+
+        const cacheKey = `${row}-${col}`
+        if (visited.has(cacheKey)) {
+            return
+        }
+        visited.add(cacheKey)
+
+        dfs(row - 1, col, visited, value)
+        dfs(row + 1, col, visited, value)
+        dfs(row, col - 1, visited, value)
+        dfs(row, col + 1, visited, value)
     }
-    
-    visited.set(key, [row, col])
-    dfs(row + 1, col, visited, currentValue)
-    dfs(row - 1, col, visited, currentValue)
-    dfs(row, col + 1, visited, currentValue)
-    dfs(row, col - 1, visited, currentValue)
-  }
 
-  const rows = heights.length
-  const cols = heights[0].length
-
-  for (let i = 0; i < cols; i++) {
-    dfs(0, i, pacific, heights[0][i])
-    dfs(rows - 1, i, atlantic, heights[rows - 1][i])
-  }
-  for (let i = 0; i < rows; i++) {
-    dfs(i, 0, pacific, heights[i][0])
-    dfs(i, cols - 1, atlantic, heights[i][cols - 1])
-  }
-
-  const answer = []
-
-  for (const [key, value] of pacific.entries()) {
-    if (atlantic.has(key)) {
-      answer.push(value)
+    for (let i = 0; i < n; i++) {
+        dfs(0, i, visitedPacific, heights[0][i])
+        dfs(m-1, i, visitedAtlantic, heights[m-1][i])
     }
-  }
 
-  return answer
+    for (let i = 0; i < m; i++) {
+        dfs(i, 0, visitedPacific, heights[i][0])
+        dfs(i, n-1, visitedAtlantic, heights[i][n-1])
+    }
+
+    const answer = []
+
+    for (const value of visitedPacific.values()) {
+        if (visitedAtlantic.has(value)) {
+            const [row, col] = value.split('-')
+            answer.push([row, col])
+        }
+    }
+
+    return answer
 };
