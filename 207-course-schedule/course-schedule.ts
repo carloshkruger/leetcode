c458e8505c3b876d940d626528b1cf3828b886e1
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-28 13:06:24 -0300

    Time: 148 ms (13.42%) | Memory: 49 MB (49.69%) - LeetSync

--- 207-course-schedule/course-schedule.ts
+++ 207-course-schedule/course-schedule.ts
@@ -1,32 +1,37 @@
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
-    const indegree: number[] = new Array(numCourses).fill(0);
-    const adj: number[][] = new Array(numCourses).fill([]).map(() => []);
+  if (!prerequisites.length) {
+    return true
+  }
 
-    for (const prerequisite of prerequisites) {
-        adj[prerequisite[1]].push(prerequisite[0]);
-        indegree[prerequisite[0]] += 1;
+  const indegree: number[] = new Array(numCourses).fill(0);
+  const graph: number[][] = new Array(numCourses).fill([]).map(() => []);
+
+  for (const prerequisite of prerequisites) {
+    graph[prerequisite[1]].push(prerequisite[0]);
+    indegree[prerequisite[0]] += 1;
+  }
+
+  const sourcesQueue = []
+  for (let i = 0; i < numCourses; i++) {
+    if (indegree[i] == 0) {
+      sourcesQueue.push(i);
     }
+  }
 
-    const queue: number[] = [];
+  let count = 0
 
-    for (let i = 0; i < numCourses; i++) {
-        if (indegree[i] == 0) {
-            queue.push(i);
-        }
+  while (sourcesQueue.length) {
+    const current = sourcesQueue.shift()
+    count++
+
+    for (const child of graph[current]) {
+      indegree[child]--
+      if (indegree[child] === 0) {
+        sourcesQueue.push(child)
+      }
     }
+  }
 
-    let visitedCount: number = 0;
-    while (queue.length > 0) {
-        const node: number = queue.shift();
-        visitedCount += 1;
-
-        for (const neighbor of adj[node]) {
-            indegree[neighbor] -= 1;
-            if (indegree[neighbor] == 0) {
-                queue.push(neighbor);
-            }
-        }
-    }
-
-    return visitedCount == numCourses;
+  return count === numCourses
 };
+
