083d45a37854bf26fb9c2e7e529095321247f8af
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-29 10:11:23 -0300

    Time: 98 ms (5.59%) | Memory: 44.6 MB (28.13%) - LeetSync

--- 62-unique-paths/unique-paths.ts
+++ 62-unique-paths/unique-paths.ts
@@ -1,14 +1,22 @@
-function uniquePaths(m: number, n: number, cache = {}): number {
-  const cacheKey = `${m}-${n}`
-  if (cacheKey in cache) {
-    return cache[cacheKey]
-  }
-  if (m === 1 && n === 1) {
-    return 1
-  }
-  if (m < 1 || n < 1) {
-    return 0
-  }
-  cache[cacheKey] = uniquePaths(m, n - 1, cache) + uniquePaths(m - 1, n, cache)
-  return cache[cacheKey]
+function uniquePaths(m: number, n: number): number {
+    const cache = new Map()
+
+    function dp(m: number, n: number): number {
+        if (m < 0 || n < 0) {
+            return 0
+        }
+        if (m === 0 && n === 0) {
+            return 1
+        }
+        const key = `${m}-${n}`
+        if (cache.has(key)) {
+            return cache.get(key)
+        }
+        
+        const result = dp(m - 1, n) + dp(m, n - 1)
+        cache.set(key, result)
+        return result
+    }
+
+    return dp(m-1, n-1)
 };
