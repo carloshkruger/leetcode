1bf40e5fcbd8347359e57645332e13fb96528d11
Carlos Henrique Kruger <carloshkruger@gmail.com>
2023-12-29 09:47:10 -0300

    Time: 54 ms (56.31%) | Memory: 43.2 MB (33.59%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.ts
+++ 54-spiral-matrix/spiral-matrix.ts
@@ -1,32 +1,32 @@
 function spiralOrder(matrix: number[][]): number[] {
-  let top = 0
-  let right = matrix[0].length - 1
-  let bottom = matrix.length - 1
   let left = 0
-  
+  let right = matrix[0].length - 1
+  let top = 0
+  let bottom = matrix.length - 1
+
   const answer = []
   const matrixLength = matrix.length * matrix[0].length
 
   while (answer.length < matrixLength) {
-    // right
+    // Going right
     for (let i = left; i <= right && answer.length < matrixLength; i++) {
       answer.push(matrix[top][i])
     }
     top++
 
-    // bottom
+    // Going down
     for (let i = top; i <= bottom && answer.length < matrixLength; i++) {
       answer.push(matrix[i][right])
     }
     right--
 
-    // left
+    // Going left
     for (let i = right; i >= left && answer.length < matrixLength; i--) {
       answer.push(matrix[bottom][i])
     }
     bottom--
 
-    // up
+    // Going up
     for (let i = bottom; i >= top && answer.length < matrixLength; i--) {
       answer.push(matrix[i][left])
     }
