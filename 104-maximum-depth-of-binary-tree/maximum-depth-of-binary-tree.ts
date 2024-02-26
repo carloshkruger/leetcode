3f3fa83d745aaa31c76dd22520709d3b806577e3
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-26 17:46:52 -0300

    Time: 67 ms (45.19%) | Memory: 54.5 MB (7.29%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
@@ -13,8 +13,8 @@
  */
 
 function maxDepth(root: TreeNode | null): number {
-    if (!root) {
-        return 0
-    }
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+  if (!root) {
+    return 0
+  }
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
