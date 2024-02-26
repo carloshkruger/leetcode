65668cbd60bb8273b6050378dff4dc94f0e09d0e
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-26 15:24:39 -0300

    Time: 67 ms (61.22%) | Memory: 53.3 MB (7.96%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+  const hashMap = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (hashMap.has(nums[i])) {
+      return [i, hashMap.get(nums[i])]
+    }
+    hashMap.set(target - nums[i], i)
+  }
+
+  return [-1, -1]
+};
