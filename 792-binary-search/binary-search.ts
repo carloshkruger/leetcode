0992e7c460d85f60d741185b6bcd6a4aa9f07813
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-26 16:02:32 -0300

    Time: 59 ms (67.44%) | Memory: 53.8 MB (24.04%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,20 +1,20 @@
 function search(nums: number[], target: number): number {
-    let start = 0
-    let end = nums.length - 1
+  let left = 0
+  let right = nums.length - 1
 
-    while (start <= end) {
-        const middleIndex = Math.floor((start + end) / 2)
-        const middleValue = nums[middleIndex]
+  while (left <= right) {
+    const middleIndex = Math.floor((left + right) / 2)
+    const value = nums[middleIndex]
 
-        if (middleValue === target) {
-            return middleIndex
-        }
-        if (middleValue > target) {
-            end = middleIndex - 1
-        } else {
-            start = middleIndex + 1
-        }
+    if (value === target) {
+      return middleIndex
     }
+    if (value > target) {
+      right = middleIndex - 1
+    } else {
+      left = middleIndex + 1
+    }
+  }
 
-    return -1
+  return -1
 };
