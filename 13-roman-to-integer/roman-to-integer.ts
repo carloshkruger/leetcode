f88b5d0b7fb514756f51102ead4b43a532d7a442
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-26 17:53:18 -0300

    Time: 115 ms (66.96%) | Memory: 55.3 MB (56.17%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -1,31 +1,32 @@
 function romanToInt(s: string): number {
-    const map = {
-        'I': 1,
-        'IV': 4,
-        'V': 5,
-        'IX': 9,
-        'X': 10,
-        'XL': 40,
-        'L': 50,
-        'XC': 90,
-        'C': 100,
-        'CD': 400,
-        'D': 500,
-        'CM': 900,
-        'M': 1000
-    }
+  const map = {
+    'I': 1,
+    'IV': 4,
+    'V': 5,
+    'IX': 9,
+    'X': 10,
+    'XL': 40,
+    'L': 50,
+    'XC': 90,
+    'C': 100,
+    'CD': 400,
+    'D': 500,
+    'CM': 900,
+    'M': 1000
+  }
 
-    let index = 0
-    let value = 0
-    while (index < s.length) {
-        if (map[`${s[index]}${s[index+1]}`]) {
-            value += map[`${s[index]}${s[index+1]}`]
-            index += 2
-        } else if (map[s[index]]) {
-            value += map[s[index]]
-            index += 1
-        }
+  let index = 0
+  let value = 0
+  while (index < s.length) {
+    const twoChars = s[index] + s[index + 1]
+    if (twoChars in map) {
+      value += map[twoChars]
+      index += 2
+    } else {
+      value += map[s[index]]
+      index += 1
     }
+  }
 
-    return value
+  return value
 };
