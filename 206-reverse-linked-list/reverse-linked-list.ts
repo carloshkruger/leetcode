cd68ff7ce55ccd1435028a9554ee1fdd055275a6
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-26 17:35:54 -0300

    Time: 61 ms (66.76%) | Memory: 52.2 MB (59.58%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.ts
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -11,15 +11,15 @@
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-    let prev = null
-    let current = head
-    
-    while (current) {
-        const temp = current.next
-        current.next = prev
-        prev = current
-        current = temp
-    }
+  let prev = null
+  let current = head
+  
+  while (current) {
+    const temp = current.next
+    current.next = prev
+    prev = current
+    current = temp
+  }
 
-    return prev
+  return prev
 };
