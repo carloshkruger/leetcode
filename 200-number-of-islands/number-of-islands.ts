6e23557642d68fc7ee51e2c2f539d89cc2abfa9c
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-27 15:58:59 -0300

    Time: 100 ms (38.58%) | Memory: 61.6 MB (22.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.ts
+++ 200-number-of-islands/number-of-islands.ts
@@ -1,13 +1,12 @@
 function numIslands(grid: string[][]): number {
-  let count = 0
   const visited = new Set()
 
-  function dfs(row: number, col: number) {
-    const isOutOfBounds = row < 0
-      || row >= grid.length
-      || col < 0
-      || col >= grid[0].length
-    if (isOutOfBounds) {
+  function bfs(row: number, col: number): boolean {
+    if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) {
+      return false
+    }
+
+    if (grid[row][col] === '0') {
       return false
     }
 
@@ -17,21 +16,19 @@ function numIslands(grid: string[][]): number {
     }
     visited.add(cacheKey)
 
-    if (grid[row][col] === '0') {
-      return false
-    }
-
-    dfs(row + 1, col)
-    dfs(row - 1, col)
-    dfs(row, col + 1)
-    dfs(row, col - 1)
+    bfs(row + 1, col)
+    bfs(row - 1, col)
+    bfs(row, col + 1)
+    bfs(row, col - 1)
 
     return true
   }
 
+  let count = 0
+
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[0].length; col++) {
-      if (dfs(row, col)) {
+      if (bfs(row, col)) {
         count++
       }
     }
