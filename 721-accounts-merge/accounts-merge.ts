692c4a0e17a0eff0441af6f7c2cb648c0ddb1883
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-27 18:10:17 -0300

    Time: 260 ms (31.17%) | Memory: 69 MB (15.58%) - LeetSync

--- 721-accounts-merge/accounts-merge.ts
+++ 721-accounts-merge/accounts-merge.ts
@@ -1,49 +1,46 @@
 function accountsMerge(accounts: string[][]): string[][] {
   const graph = {}
-  const nameMap = {}
+  const owners = {}
 
   for (const [name, ...emails] of accounts) {
     for (const email of emails) {
-      if (!(email in graph)) {
-        graph[email] = []
-        nameMap[email] = name
-      }
+      owners[email] = name
+
       for (const email2 of emails) {
-        if (email !== email2) {
-          graph[email].push(email2)
+        if (!(email2 in graph)) {
+          graph[email2] = []
+        }
+        graph[email].push(email2)
+      }
+    }
+  }
+  
+  const visited = new Set()
+  const list = []
+
+  for (const key in graph) {
+    if (visited.has(key)) {
+      continue
+    }
+    const currentMerge = new Set()
+    const stack = [key]
+
+    while (stack.length) {
+      const item = stack.pop()
+      visited.add(item)
+      currentMerge.add(item)
+
+      for (const neighbor of graph[item]) {
+        if (!visited.has(neighbor)) {
+          stack.push(neighbor)
         }
       }
     }
+
+    const currentMergeArray = [...currentMerge.values()].sort()
+
+    list.push([owners[key], ...currentMergeArray])
   }
 
-  const visited = new Set()
-  const groupedEmails = {}
-  const answer = []
-
-  const groupEmails = (originalEmail: string, currentEmail: string) => {
-    if (visited.has(currentEmail)) {
-      return
-    }
-    groupedEmails[originalEmail].push(currentEmail)
-    visited.add(originalEmail)
-    visited.add(currentEmail)
-    for (const childEmail of graph[currentEmail]) {
-      groupEmails(originalEmail, childEmail)
-    }
-  }
-
-  for (const email in graph) {
-    if (!visited.has(email)) {
-      if (!(email in groupedEmails)) {
-        groupedEmails[email] = []
-      }
-      groupEmails(email, email)
-    }
-  }
-
-  for (const email in groupedEmails) {
-    answer.push([nameMap[email], ...groupedEmails[email].sort()])
-  }
-
-  return answer
+  return list
 };
