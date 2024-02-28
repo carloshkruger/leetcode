90a9ac05ccc83a9e12a45a9576cab3bfd9c41878
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-28 15:00:30 -0300

    Time: 208 ms (81.63%) | Memory: 75 MB (31.35%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.ts
+++ 739-daily-temperatures/daily-temperatures.ts
@@ -1,20 +1,21 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-    const stack = []
-    const answer = []
+  const stack: { value: number, index: number }[] = []
+  const answer = []
 
-    for (let i = temperatures.length - 1; i >= 0; i--) {
-        const current = temperatures[i]
-        while (stack.length && current >= stack.at(-1).value) {
-            stack.pop()
-        }
-        if (stack.length === 0) {
-            answer.push(0)
-        } else {
-            answer.push(stack.at(-1).index - i)
-        }
-
-        stack.push({ value: current, index: i })
+  for (let i = temperatures.length - 1; i >= 0; i--) {
+    while (stack.length && stack.at(-1).value <= temperatures[i]) {
+      stack.pop()
     }
 
-    return answer.reverse()
+    if (stack.length === 0) {
+      answer.push(0)
+    } else {
+      const distance = stack.at(-1).index - i
+      answer.push(distance)
+    }
+
+    stack.push({ value: temperatures[i], index: i })
+  }
+
+  return answer.reverse()
 };
