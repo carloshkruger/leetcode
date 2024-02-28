c30588fb88ed0b50fdfbd903a7a196b092106bf4
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-02-28 15:07:37 -0300

    Time: 75 ms (52.89%) | Memory: 53.5 MB (47.11%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.ts
+++ 36-valid-sudoku/valid-sudoku.ts
@@ -1,36 +1,35 @@
 function isValidSudoku(board: string[][]): boolean {
-    const rowSet = new Set()
-    const colSet = new Set()
-    const boxSet = new Set()
+  const rowSet = new Set()
+  const colSet = new Set()
+  const subBoxSet = new Set()
 
-    for (let row = 0; row < board.length; row++) {
-        const subboxRowIndex = Math.floor(row / 3)
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      if (board[row][col] === '.') {
+        continue
+      }
 
-        for (let col = 0; col < board[0].length; col++) {
-            if (board[row][col] === '.') {
-                continue
-            }
+      const rowKey = `${row}-${board[row][col]}`
+      if (rowSet.has(rowKey)) {
+        return false
+      }
+      rowSet.add(rowKey)
 
-            const rowKey = `${row}-${board[row][col]}`
-            if (rowSet.has(rowKey)) {
-                return false
-            }
-            rowSet.add(rowKey)
+      const colKey = `${col}-${board[row][col]}`
+      if (colSet.has(colKey)) {
+        return false
+      }
+      colSet.add(colKey)
 
-            const colKey = `${col}-${board[row][col]}`
-            if (colSet.has(colKey)) {
-                return false
-            }
-            colSet.add(colKey)
-
-            const subboxColIndex = Math.floor(col / 3)
-            const subboxKey = `${subboxRowIndex}-${subboxColIndex}-${board[row][col]}`
-            if (boxSet.has(subboxKey)){
-                return false
-            }
-            boxSet.add(subboxKey)
-        }
+      const subBoxRow = Math.floor(row / 3)
+      const subBoxCol = Math.floor(col / 3)
+      const subBoxKey = `${subBoxRow}-${subBoxCol}-${board[row][col]}`
+      if (subBoxSet.has(subBoxKey)) {
+        return false
+      }
+      subBoxSet.add(subBoxKey)
     }
+  }
 
-    return true
+  return true
 };
