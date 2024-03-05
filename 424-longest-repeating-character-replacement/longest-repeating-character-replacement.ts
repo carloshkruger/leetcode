748fa4e5696b8eddbbc89a9b43e90d9f020ad287
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-05 14:26:12 -0300

    Time: 75 ms (63.41%) | Memory: 52.7 MB (64.18%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.ts
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.ts
@@ -1,19 +1,22 @@
 function characterReplacement(s: string, k: number): number {
-  const hashCount = new Map()
-  let maxRepeatLetterCount = 0
-  let maxLength = 0
+  let maxLetterCount = 0
   let windowStart = 0
+  let map = new Map()
+  let maxLength = 0
 
   for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
-    hashCount.set(s[windowEnd], (hashCount.get(s[windowEnd]) ?? 0) + 1)
-    maxRepeatLetterCount = Math.max(maxRepeatLetterCount, hashCount.get(s[windowEnd]))
-
-    if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
-      hashCount.set(s[windowStart], hashCount.get(s[windowStart]) - 1)
+    const count = (map.get(s[windowEnd]) ?? 0) + 1
+  
+    map.set(s[windowEnd], count)
+    maxLetterCount = Math.max(maxLetterCount, count)
+    
+    if (windowEnd - windowStart + 1 - maxLetterCount > k) {
+      const newCount = map.get(s[windowStart]) - 1
+      map.set(s[windowStart], newCount)
       windowStart++
     }
-  
-    maxLength = Math.max(maxLength, windowEnd - windowStart + 1)
+
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1)  
   }
 
   return maxLength
