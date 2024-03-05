de46d9dee033dd5fa383bece2c8712d8f2d66cce
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-05 11:44:19 -0300

    Time: 62 ms (98.82%) | Memory: 54.1 MB (68.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,16 +1,14 @@
 function lengthOfLongestSubstring(s: string): number {
-  const visited = new Set()
-  let leftIndex = 0
+  const map = new Map()
+  let startWindow = 0
   let longest = 0
 
   for (let i = 0; i < s.length; i++) {
-    while (visited.has(s[i])) {
-      visited.delete(s[leftIndex])
-      leftIndex++
+    if (map.has(s[i])) {
+      startWindow = Math.max(startWindow, map.get(s[i]) + 1)
     }
-    
-    visited.add(s[i])
-    longest = Math.max(longest, visited.size)
+    longest = Math.max(longest, i - startWindow + 1)
+    map.set(s[i], i)
   }
 
   return longest
