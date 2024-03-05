ce12e4f61d518b762d998702bc92b6cc4176eab3
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-05 16:10:31 -0300

    Time: 61 ms (85.90%) | Memory: 52.9 MB (50.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,11 +1,6 @@
 function removeDuplicates(nums: number[]): number {
-  if (!nums.length) {
-    return 0
-  }
-
   let slowPointer = 1
   let currentValue = nums[0]
-  
   for (let fastPointer = 1; fastPointer < nums.length; fastPointer++) {
     if (nums[fastPointer] !== currentValue) {
       currentValue = nums[fastPointer]
@@ -13,6 +8,5 @@ function removeDuplicates(nums: number[]): number {
       slowPointer++
     }
   }
-
   return slowPointer
 };
