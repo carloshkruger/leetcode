cd7089fbd4f30956571eee469356592978a9aadb
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-05 15:49:03 -0300

    Time: 85 ms (70.24%) | Memory: 53.7 MB (59.39%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.ts
+++ 76-minimum-window-substring/minimum-window-substring.ts
@@ -1,47 +1,38 @@
 function minWindow(s: string, t: string): string {
-  if (t.length > s.length) {
-    return ''
+  const tCount = new Map
+  for (const char of t) {
+    tCount.set(char, (tCount.get(char) ?? 0) + 1)
   }
 
-  const tHashCount = new Map()
-  for (const letter of t) {
-    tHashCount.set(letter, (tHashCount.get(letter) ?? 0) + 1)
-  }
-
-  let smalestLength = Infinity
-  let substrStart = 0
   let windowStart = 0
   let matches = 0
+  let minChar = ''
+  let minLength = Infinity
 
   for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
-    const letter = s[windowEnd]
-    if (tHashCount.has(letter)) {
-      const count = tHashCount.get(letter)
-      if (count === 1) {
+    const char = s[windowEnd]
+    if (tCount.has(char)) {
+      tCount.set(char, tCount.get(char) - 1)
+      if (tCount.get(char) === 0) {
         matches++
       }
-      tHashCount.set(letter, count - 1)
     }
-  
-    while (matches === tHashCount.size) {
-      if (windowEnd - windowStart + 1 < smalestLength) {
-        smalestLength = windowEnd - windowStart + 1
-        substrStart = windowStart
+
+    while (matches === tCount.size && windowStart <= windowEnd) {
+      if (windowEnd - windowStart + 1 < minLength) {
+        minLength = windowEnd - windowStart + 1
+        minChar = s.substring(windowStart, windowEnd + 1)
       }
-      if (tHashCount.has(s[windowStart])) {
-        const count = tHashCount.get(s[windowStart])
-        tHashCount.set(s[windowStart], count + 1)
-        if (count >= 0) {
+
+      if (tCount.has(s[windowStart])) {
+        if (tCount.get(s[windowStart]) === 0) {
           matches--
         }
+        tCount.set(s[windowStart], tCount.get(s[windowStart]) + 1)
       }
       windowStart++
     }
   }
 
-  if (smalestLength === Infinity) {
-    return ''
-  }
-
-  return s.substring(substrStart, substrStart + smalestLength)
+  return minChar
 };
