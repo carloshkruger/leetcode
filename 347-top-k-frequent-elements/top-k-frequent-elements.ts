3a6175a8df55ca8916c5ffccdb60e67e12fa2be7
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-06 10:34:41 -0300

    Time: 83 ms (22.46%) | Memory: 54.3 MB (51.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,15 +1,10 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    const countMap = new Map()
-    for (const num of nums) {
-        countMap.set(num, (countMap.get(num) ?? 0) + 1)
-    }
+  const frequencyCount = new Map()
+  for (const num of nums) {
+    frequencyCount.set(num, (frequencyCount.get(num) ?? 0) + 1)
+  }
 
-    const orderedList = [...countMap.entries()].sort((a, b) => b[1] - a[1])
-    const answer = []
+  const frequencyList = [...frequencyCount.entries()].sort((a,b) => b[1] - a[1])
 
-    for (let i = 0; i < k; i++) {
-        answer.push(orderedList[i][0])
-    }
-
-    return answer
+  return frequencyList.slice(0, k).map(item => item[0])
 };
