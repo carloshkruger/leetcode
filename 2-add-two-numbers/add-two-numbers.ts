10e23123a9c177a36c13c302fca2ee89ee868669
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-06 15:39:14 -0300

    Time: 103 ms (79.23%) | Memory: 56.7 MB (76.57%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -10,29 +10,29 @@
  * }
  */
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null, carry = 0): ListNode | null {
-  if (!l1 && !l2) {
-    return null
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  function helper(l1: ListNode | null, l2: ListNode | null, carry: number): ListNode | null {
+    if (!l1 && !l2) {
+      return null
+    }
+
+    let value = (l1?.val ?? 0) + (l2?.val ?? 0) + carry
+    if (value > 9) {
+      carry = Math.floor(value / 10)
+    } else {
+      carry = 0
+    }
+    value = value % 10
+
+    const node = new ListNode(value)
+    node.next = helper(l1?.next || null, l2?.next || null, carry)
+
+    if (!node.next && carry > 0) {
+      node.next = new ListNode(carry)
+    }
+
+    return node
   }
 
-  const num1 = l1?.val ?? 0
-  const num2 = l2?.val ?? 0
-  let sum = num1 + num2 + carry
-
-  if (sum > 9) {
-    sum = sum % 10
-    carry = 1
-  } else if (carry === 1) {
-    carry = 0
-  }
-
-  const node = new ListNode(sum)
-  node.next = addTwoNumbers(l1?.next || null, l2?.next || null, carry)
-
-  if (!node.next && carry === 1) {
-    node.next = new ListNode(1)
-  }
-
-  return node
+  return helper(l1, l2, 0)
 };
-
