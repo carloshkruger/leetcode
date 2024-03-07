452f743e48c90ff1e904a9e9c280812cf5658237
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-07 10:51:44 -0300

    Time: 102 ms (20.65%) | Memory: 60 MB (34.78%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.ts
+++ 1028-interval-list-intersections/interval-list-intersections.ts
@@ -1,13 +1,21 @@
 function intervalIntersection(firstList: number[][], secondList: number[][]): number[][] {
   const result = []
+  let i = 0
+  let j = 0
 
-  for (let i = 0; i < firstList.length; i++) {
-    for (let j = 0; j < secondList.length; j++) {
-      if (firstList[i][0] <= secondList[j][1] && firstList[i][1] >= secondList[j][0]) {
-        const start = Math.max(firstList[i][0], secondList[j][0])
-        const end = Math.min(firstList[i][1], secondList[j][1])
-        result.push([start, end])
-      }
+  while (i < firstList.length && j < secondList.length) {
+    const hasOverlap = firstList[i][0] <= secondList[j][1] && firstList[i][1] >= secondList[j][0]
+
+    if (hasOverlap) {
+      const start = Math.max(firstList[i][0], secondList[j][0])
+      const end = Math.min(firstList[i][1], secondList[j][1])
+      result.push([start, end])
+    }
+
+    if (firstList[i][1] < secondList[j][1]) {
+      i++
+    } else {
+      j++
     }
   }
 
