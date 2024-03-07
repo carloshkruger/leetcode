04b28b52dd82fa3b14ab34a5ba7ab459eb2d609f
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-07 10:20:52 -0300

    Time: 93 ms (12.87%) | Memory: 55.7 MB (25.59%) - LeetSync

--- 57-insert-interval/insert-interval.ts
+++ 57-insert-interval/insert-interval.ts
@@ -1,21 +1,25 @@
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-  const newList = []
-  let index = 0
+  intervals.sort((a, b) => a[0] - b[0])
 
-  while (index < intervals.length && newInterval[0] > intervals[index][1]) {
-    newList.push(intervals[index])
+  const list = []
+
+  let index = 0
+  while (index < intervals.length && intervals[index][1] < newInterval[0]) {
+    list.push([intervals[index][0], intervals[index][1]])
     index++
   }
+
   while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
     newInterval[0] = Math.min(newInterval[0], intervals[index][0])
     newInterval[1] = Math.max(newInterval[1], intervals[index][1])
     index++
   }
-  newList.push(newInterval)
+  list.push([newInterval[0], newInterval[1]])
+
   while (index < intervals.length) {
-    newList.push(intervals[index])
+    list.push([intervals[index][0], intervals[index][1]])
     index++
   }
 
-  return newList
+  return list
 };
