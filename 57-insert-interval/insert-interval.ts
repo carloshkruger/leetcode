ebdcd28ba69958af293cdbf7bfccc48454dedea3
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-07 10:24:14 -0300

    Time: 88 ms (24.41%) | Memory: 54.3 MB (72.34%) - LeetSync

--- 57-insert-interval/insert-interval.ts
+++ 57-insert-interval/insert-interval.ts
@@ -1,25 +1,26 @@
 function insert(intervals: number[][], newInterval: number[]): number[][] {
+  intervals.push([newInterval[0], newInterval[1]])
+
+  if (intervals.length < 2) {
+    return intervals
+  }
+
   intervals.sort((a, b) => a[0] - b[0])
 
-  const list = []
-
-  let index = 0
-  while (index < intervals.length && intervals[index][1] < newInterval[0]) {
-    list.push([intervals[index][0], intervals[index][1]])
-    index++
+  const mergedList = []
+  
+  let start = intervals[0][0]
+  let end = intervals[0][1]
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] > end) {
+      mergedList.push([start, end])
+      start = intervals[i][0]
+      end = intervals[i][1]
+    } else {
+      end = Math.max(end, intervals[i][1])
+    }
   }
+  mergedList.push([start, end])
 
-  while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
-    newInterval[0] = Math.min(newInterval[0], intervals[index][0])
-    newInterval[1] = Math.max(newInterval[1], intervals[index][1])
-    index++
-  }
-  list.push([newInterval[0], newInterval[1]])
-
-  while (index < intervals.length) {
-    list.push([intervals[index][0], intervals[index][1]])
-    index++
-  }
-
-  return list
+  return mergedList
 };
