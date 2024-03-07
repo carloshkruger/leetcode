62f886e59e7e4887715426147df76490ab845e77
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-07 15:30:49 -0300

    Time: 69 ms (8.75%) | Memory: 52 MB (12.36%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.ts
+++ 226-invert-binary-tree/invert-binary-tree.ts
@@ -16,8 +16,10 @@ function invertTree(root: TreeNode | null): TreeNode | null {
   if (!root) {
     return null
   }
+
   const left = root.left
   root.left = invertTree(root.right)
   root.right = invertTree(left)
+
   return root
 };
