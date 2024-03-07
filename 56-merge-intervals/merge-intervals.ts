6833ebb5f4f2398d9c93ca0ebdc9ce8d30e3986a
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-07 10:14:16 -0300

    Time: 82 ms (96.33%) | Memory: 60.1 MB (49.77%) - LeetSync

--- 56-merge-intervals/merge-intervals.ts
+++ 56-merge-intervals/merge-intervals.ts
@@ -5,22 +5,20 @@ function merge(intervals: number[][]): number[][] {
 
   intervals.sort((a, b) => a[0] - b[0])
 
+  const mergedIntervals = []
+
   let start = intervals[0][0]
   let end = intervals[0][1]
-
-  const list = []
-
   for (let i = 1; i < intervals.length; i++) {
     if (intervals[i][0] > end) {
-      list.push([start, end])
+      mergedIntervals.push([start, end])
       start = intervals[i][0]
       end = intervals[i][1]
     } else {
       end = Math.max(end, intervals[i][1])
     }
   }
+  mergedIntervals.push([start, end])
 
-  list.push([start, end])
-
-  return list
+  return mergedIntervals
 };
