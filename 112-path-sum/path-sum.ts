db6a019db405692ee86c61f33a6c98131a80a9c0
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-08 10:07:52 -0300

    Time: 77 ms (13.30%) | Memory: 54.2 MB (58.44%) - LeetSync

--- 112-path-sum/path-sum.ts
+++ 112-path-sum/path-sum.ts
@@ -16,9 +16,9 @@ function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
   if (!root) {
     return false
   }
-  if (!root.left && !root.right && root.val === targetSum) {
+  targetSum -= root.val
+  if (!root.left && !root.right && targetSum === 0) {
     return true
   }
-  return hasPathSum(root.left, targetSum - root.val)
-    || hasPathSum(root.right, targetSum - root.val)
+  return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum)
 };
