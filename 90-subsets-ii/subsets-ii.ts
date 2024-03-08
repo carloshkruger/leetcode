94f3e210ce1c71106721ee86c2f1a87cdce4c2fa
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-03-08 14:15:03 -0300

    Time: 56 ms (97.35%) | Memory: 53.7 MB (78.31%) - LeetSync

--- 90-subsets-ii/subsets-ii.ts
+++ 90-subsets-ii/subsets-ii.ts
@@ -1,21 +1,20 @@
 function subsetsWithDup(nums: number[]): number[][] {
-    nums.sort((a, b) => a - b)
+  nums.sort((a,b) => a-b)
 
-    const result = [[]]
-    let start = 0
-    let end = 0
+  const list = [[]]
+  let listStartIndex = 0
 
-    for (let i = 0; i < nums.length; i++) {
-        end = result.length
-        for (let j = start; j < end; j++) {
-            result.push([...result[j], nums[i]])
-        }
-        if (nums[i] === nums[i + 1]) {
-            start = end
-        } else {
-            start = 0
-        }
+  for (let i = 0; i < nums.length; i++) {
+    let listLength = list.length
+    for (let j = listStartIndex; j < listLength; j++) {
+      list.push([...list[j], nums[i]])
     }
+    if (nums[i] === nums[i+1]) {
+      listStartIndex = listLength
+    } else {
+      listStartIndex = 0
+    }
+  }
 
-    return result
+  return list
 };
