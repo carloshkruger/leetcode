a7ca910022a812df68e54e1ac30a6c5b73807402
Carlos Henrique Kruger <carloshkruger@gmail.com>
2024-06-10 09:50:34 -0300

    Time: 82 ms (32.14%) | Memory: 60.1 MB (18.34%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.ts
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -16,11 +16,13 @@ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: Tree
 	if (!root) {
     return null
   }
-  if (root.val > p.val && root.val > q.val) {
+
+  if (p.val < root.val && q.val < root.val) {
     return lowestCommonAncestor(root.left, p, q)
   }
-  if (root.val < p.val && root.val < q.val) {
+  if (p.val > root.val && q.val > root.val) {
     return lowestCommonAncestor(root.right, p, q)
   }
+
   return root
 };
